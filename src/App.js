2e79deb9fb1421d75d7fae7da343313d464d0e9e
Yuuuusssuufff <ajibolayusuf8@gmail.com>
2022-05-25 16:57:06 +0100

    anecdotes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React, {useState} from 'react'
+import Anecdotes from './components/Anecdotes'
 
-function App() {
-  
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
-  
-  const randomIndex = Math.floor(Math.random()*(anecdotes.length-1))
-  const [selected, setSelected] = useState(anecdotes[0])
-
-
-  const style = {
-   borderRadius: '5px', 
-   border: "1px solid yellow"
+export default function App() {
+return <div>
+  <Anecdotes />
+  </div>
   }
 
-function handleClick(){
-    setSelected(anecdotes[randomIndex])
-}
-
-  return (
-    <div>
-      {selected} <br />
-      <button onClick = {handleClick} style = {style}>next anecdote</button>
-    </div>
-  )
-  
-}
-
-export default App
+// export default App
 
 // export default function App() {
 //   //state
